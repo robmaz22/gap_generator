4895055d11461b0d41252bba1c2d46017b9ae165
robmaz22 <czowieg@gmail.com>
2021-06-10 22:57:40 +0200

    Dodanie menu z funkcjami Nowy i Otwórz

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import random
 from tkinter import *
 from tkinter import messagebox
 from stop_words import get_stop_words
+from tkinter import filedialog
+
 
 def select_all(event):
     text_box.tag_add(SEL, "1.0", END)
     text_box.mark_set(INSERT, "1.0")
     text_box.see(INSERT)
     return 'break'
+
 
 def run():
     stop_words = get_stop_words('pl')
@@ -17,7 +20,7 @@ def run():
     splitted = content.split()
 
     try:
-        gap_number = int(entry1.get()) * 2
+        gap_number = int(entry1.get())
     except Exception:
         messagebox.showerror('Błąd', 'Błędna liczba luk!')
         return
@@ -29,15 +32,16 @@ def run():
         return
 
     gaped = []
+    cycle = 0
 
-    while gap_number > 0:
+    while gap_number > 0 and cycle < len(splitted):
         gap = random.randint(1, len(splitted) - 2)
         if gap not in gaped:
             if gap < len(splitted):
 
                 word = splitted[gap]
-                word = word.replace('.','')
-                word = word.replace(',','')
+                word = word.replace('.', '')
+                word = word.replace(',', '')
                 word = word.lower()
 
                 if word not in stop_words and len(word) > 1:
@@ -53,12 +57,18 @@ def run():
                             splitted[gap] = '........'
                             gaped.append(gap)
                         gap_number -= 1
-
+        cycle += 1
 
     joined = ' '.join(splitted)
     text_box.delete(1.0, END)
     text_box.insert(1.0, joined)
     messagebox.showinfo('Sukces', f'Wygenerowano {gap_gen} luk!')
+
+
+def new_text():
+    choice = messagebox.askquestion('Tworzenie nowego tekstu', 'Czy chcesz utworzyć nowy tekst?')
+    if choice == 'yes':
+        text_box.delete(1.0, END)
 
 
 root = Tk()
@@ -68,12 +78,49 @@ screen_width = int((root.winfo_screenwidth() / 2))
 screen_height = int((root.winfo_screenheight() / 2) + 200)
 root.geometry(f'{screen_width}x{screen_height}')
 
+
+def open_text():
+    path = filedialog.askopenfilename(title='Wybierz plik',
+                                      filetypes=(("Plik tekstowy", "*.txt"), ("Wszystkie pliki", "*.*")))
+
+    try:
+        with open(path, 'r') as file:
+            content = file.read()
+    except Exception:
+        return
+
+    text_box.delete(1.0, END)
+    text_box.insert(1.0, content)
+
+
+def ask_quit():
+    if len(text_box.get(1.0, END)) > 1:
+        ask = messagebox.askyesno("Wyjście", "Pole tekstowe nie jest puste. Czy na pewno chcesz wyjść?")
+        if ask == 1:
+            root.destroy()
+    else:
+        root.destroy()
+
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Nowy", command=new_text)
+filemenu.add_command(label="Otwórz", command=open_text)
+filemenu.add_command(label="Zapisz jako")
+menubar.add_cascade(label="Plik", menu=filemenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="O programie")
+menubar.add_cascade(label="Pomoc", menu=helpmenu)
+
+root.config(menu=menubar)
+
 scrollbar = Scrollbar(root)
 scrollbar.grid(row=0, column=3, sticky=NS)
 
 text_box = Text(root, wrap=WORD, yscrollcommand=scrollbar.set,
-                width=int(screen_width/6)-30,
-                height=int(screen_height/20)-2)
+                width=int(screen_width / 6) - 30,
+                height=int(screen_height / 20) - 2)
 
 scrollbar.config(command=text_box.yview)
 text_box.grid(row=0, column=0, columnspan=2)
@@ -82,19 +129,20 @@ text_box.bind("<Control-Key-A>", select_all)
 
 label = Label(root, text='Liczba luk', font=("Arial", 12))
 label.grid(row=1, column=0, pady=5, sticky=E, padx=2)
-entry1 = Entry(root, width=int(screen_width/200), font=("Arial 11"))
-entry1.grid(row=1, column=1, pady=5, sticky=W, ipady=int(screen_height/200))
+entry1 = Entry(root, width=int(screen_width / 200), font=("Arial 11"))
+entry1.grid(row=1, column=1, pady=5, sticky=W, ipady=int(screen_height / 200))
 entry1.insert(1, '10')
 
 run_btn = Button(root, text='Generuj',
                  command=run,
                  font=("Arial", 12),
-                 width=int(screen_width/50),
-                 height=int(screen_height/300))
+                 width=int(screen_width / 50),
+                 height=int(screen_height / 300))
 run_btn.grid(row=2, column=0, columnspan=3, pady=10)
 
 version = 'Autor: Robert\nWersja 2.0'
 label1 = Label(root, text=version, font=("Arial", 9))
 label1.grid(row=3, column=1, sticky=E)
 
+root.protocol("WM_DELETE_WINDOW", ask_quit)
 root.mainloop()
