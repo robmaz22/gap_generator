f5d305682bee96f1966466187442c16631094196
robmaz22 <czowieg@gmail.com>
2021-06-13 19:05:00 +0200

    Dodanie funkcji zmiany czcionki i rozmiaru

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import random
 from tkinter import *
 from tkinter import messagebox
+from tkinter.ttk import Combobox
+from tkinter import font
 from stop_words import get_stop_words
 from tkinter import filedialog
 
 
 class App:
     def __init__(self, master):
+        self.version = 3.0
         self.master = master
         self.master.title('GENERATOR LUK')
         self.window_width = int((root.winfo_screenwidth() / 2))
@@ -15,6 +18,10 @@ class App:
         Grid.rowconfigure(root, index=0, weight=1)
         Grid.columnconfigure(root, index=0, weight=1)
         self.master.minsize(300, 300)
+        self.font = 'Arial'
+        self.font_size = 11
+        self.size_idx = 1
+        self.font_idx = 1
 
         self.menubar = Menu(self.master)
         self.filemenu = Menu(self.menubar, tearoff=0)
@@ -22,6 +29,10 @@ class App:
         self.filemenu.add_command(label="Otwórz", command=self.open_text)
         self.filemenu.add_command(label="Zapisz jako", command=self.save_file)
         self.menubar.add_cascade(label="Plik", menu=self.filemenu)
+
+        self.option_menu = Menu(self.menubar, tearoff=0)
+        self.option_menu.add_command(label="Tekst", command=self.config_text)
+        self.menubar.add_cascade(label="Opcje", menu=self.option_menu)
 
         self.helpmenu = Menu(self.menubar, tearoff=0)
         self.helpmenu.add_command(label="O programie", command=self.info)
@@ -58,7 +69,7 @@ class App:
         self.master.bind('<Configure>', self.resize_text)
         self.master.protocol("WM_DELETE_WINDOW", self.ask_quit)
 
-    def select_all(self):
+    def select_all(self, event):
         self.text_box.tag_add(SEL, "1.0", END)
         self.text_box.mark_set(INSERT, "1.0")
         self.text_box.see(INSERT)
@@ -110,6 +121,17 @@ class App:
                                 gaped.append(gap)
                             gap_number -= 1
             cycle += 1
+
+        idx = 0
+
+        while idx < len(splitted) - 1:
+            word = splitted[idx]
+
+            if word == '*':
+                splitted.insert(idx, '\n')
+                idx += 1
+
+            idx += 1
 
         joined = ' '.join(splitted)
         self.text_box.delete(1.0, END)
@@ -163,7 +185,62 @@ class App:
         info_window = Toplevel(self.master)
         info_window.resizable(False, False)
         info_window.title("O programie")
-        Label(info_window, text="Program do generowania luk w tekscie.\nAutor: Robert\nWersja: 2.7").pack()
+        Label(info_window, text=f"Program do generowania luk w tekscie.\nAutor: Robert\nWersja: {self.version}").pack()
+
+    def config_text(self):
+        self.text_window = Toplevel(self.master)
+        self.text_window.resizable(False, False)
+        self.text_window.title("Ustawienia tekstu")
+
+        Label(self.text_window, text="Czcionka:").grid(column=0, row=1, padx=10, pady=5)
+        Label(self.text_window, text="Rozmiar tekstu:").grid(column=0, row=2, padx=10, pady=5)
+
+        n = IntVar()
+        m = StringVar()
+
+        self.cbox1 = Combobox(self.text_window, width=27)
+        self.cbox2 = Combobox(self.text_window, width=27)
+
+        self.fonts = font.families()
+        self.sizes = list(range(8, 72, 3))
+
+        self.cbox1['values'] = self.sizes
+        self.cbox2['values'] = self.fonts
+
+        self.cbox1.grid(column=1, row=2)
+        self.cbox2.grid(column=1, row=1)
+
+        self.cbox1.current(self.size_idx)
+        self.cbox2.current(self.font_idx)
+
+        # button = Button(root, text="Select color", command=choose_color)
+
+        labelframe = LabelFrame(self.text_window, text="Przykładowy tekst")
+        labelframe.grid(column=0, row=0, columnspan=2)
+
+        self.example = Label(labelframe, text="Ala ma kota.")
+        self.example.pack()
+
+        self.cbox1.bind("<<ComboboxSelected>>", self.change_example)
+        self.cbox2.bind("<<ComboboxSelected>>", self.change_example)
+
+        btn = Button(self.text_window, text='Zatwierdź', command=self.change_font).grid(column=0, row=3, columnspan=2,
+                                                                                        pady=15)
+
+    def change_example(self, event):
+        self.font_size = self.cbox1.get()
+        self.font= self.cbox2.get()
+        self.example.config(font=(self.font, self.font_size))
+
+    def change_font(self):
+        self.text_box.config(font=(self.font, self.font_size))
+        try:
+            self.size_idx = self.sizes.index(int(self.font_size))
+            self.font_idx = self.fonts.index(self.font)
+        except Exception:
+            self.size_idx = 1
+            self.font_idx = 1
+        self.text_window.destroy()
 
 
 if __name__ == '__main__':
